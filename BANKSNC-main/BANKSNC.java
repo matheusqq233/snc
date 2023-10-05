6e91e747b64f6a05f8af97b2add1e53ebb43fd01
matheusqq233 <117654021+matheusqq233@users.noreply.github.com>
2023-10-05 15:54:22 -0300

    Add files via upload

---
+++ BANKSNC-main/BANKSNC.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exemplojava;
+
+
+import java.util.Scanner;
+/**
+ *
+ * @author 182210039
+ */
+public class BANKSNC {
+     public static void main(String[] args) {
+        
+      Scanner read = new Scanner(System.in);
+ 
+ 
+
+         System.out.println("******************  *******************");        
+         System.out.println("******************  *******************");        
+         System.out.println("              BANCKSNC\")");
+         System.out.println("******************  *******************");        
+         System.out.println("******************  *******************");        
+         System.out.println(" ");
+         System.out.println(" ");
+         System.out.println("[1] - Login");   
+         System.out.println(" ");
+         System.out.println(" ");
+         System.out.println("[2] - Cadastro");
+         System.out.println(" ");
+         System.out.println(" ");
+         System.out.println("[3] - Sair");
+         System.out.println(" ");
+         System.out.print("DIGITE SUA OPÇÃO = ");
+         int menu = read.nextInt();
+        
+         
+                
+}
+     }
+
